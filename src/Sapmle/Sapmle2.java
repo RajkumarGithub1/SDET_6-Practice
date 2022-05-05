87bb9bc8b13a917d5a379495d6d44c1277549a7d
DELL <DELL@DESKTOP-68OQ1K7 rajkumar>
2022-05-05 16:28:42 +0530

    sample2 added

---
+++ src/Sapmle/Sapmle2.java
@@ -0,0 +1,16 @@
+package Sapmle;
+
+public class Sapmle2 
+{
+	
+	public static void main(String[] args) 
+	{
+		String a="Rajkumar J";
+		
+		int size = a.length();
+		
+		System.out.println(size);
+	
+		
+	}
+}
